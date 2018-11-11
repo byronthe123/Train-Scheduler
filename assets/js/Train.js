57ee86148b55bf0366a81ac967fe96bd5510d77a
Aedirn <byroninjeeli@gmail.com>
2018-11-10 23:13:23 -0500

    changes

--- assets/js/Train.js
+++ assets/js/Train.js
@@ -13,7 +13,7 @@ class Train {
         let str2 = '';
         str1 += `${this.name.charAt(0)}${this.name.charAt(this.name.length-1)}`;
         str2 += `${this.destination.charAt(0)}${this.destination.charAt(this.destination.length -1)}`;
-        let id = `${str1}-${str2}-${moment()}`
+        let id = `${str1}-${str2}`
         return id;
     }
 
